3df242ee83e97c427a5d23ca8794010111cbf94a
zubairbasha <91623908+zubairbasha@users.noreply.github.com>
2022-07-17 20:26:22 +0530

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -8,5 +8,6 @@ class Simple{
      System.out.println("Fourth Command");
      System.out.println("Fifth Commit");
      System.out.println("Sixth Commit");
+        System.out.println("Seventh commit");
     }  
 }  
