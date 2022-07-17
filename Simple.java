8737490b89bef0cd137717e0c8f21595449bd40f
zubair basha <mohammedzubair006@gmail.com>
2022-07-17 17:54:21 +0530

    This is jenkins exercise

--- Simple.java
+++ Simple.java
@@ -2,5 +2,6 @@ class Simple{
     public static void main(String args[]){  
      System.out.println("Hello ");  
      System.out.println("Zubair basha");
+     System.out.println("This is Git Hub Push exercise");
     }  
 }  
